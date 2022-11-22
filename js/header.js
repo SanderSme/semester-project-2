14b249a451be37f00e6e16d2038329d615e9a1b0
Sander Smedbøl <sander.smedbol94@gmail.com>
2022-11-22 17:26:27 +0100

    add list item, signup, login and bugfix

--- js/header.js
+++ js/header.js
@@ -1,10 +1,12 @@
 const hamburgerMeny = document.querySelector('#hamburgerMenu');
 const navLinksMobile = document.querySelector('#navLinksMobile');
+const body = document.querySelector('body');
 
 hamburgerMeny.addEventListener('click', () => {
   navLinksMobile.classList.toggle('hidden');
   hamburgerMeny.classList.toggle('fa-bars');
   hamburgerMeny.classList.toggle('fa-times');
+  body.classList.toggle('overflow-hidden');
 });
 
 const searchBtn = document.querySelector('#searchBtn');
