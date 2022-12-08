d83fdb67b0cd3b4e9ca99e1325a89ebf82cf2236
Sander Smedbøl <sander.smedbol94@gmail.com>
2022-12-08 15:01:25 +0100

    fix search

--- js/components/scrollbar.js
+++ js/components/scrollbar.js
@@ -1,6 +1,12 @@
 const userListingsContainer = document.querySelector('#userListingsContainer');
+const userBidsContainer = document.querySelector("#userBidsContainer")
 
 userListingsContainer.addEventListener('wheel', (event) => {
   event.preventDefault();
   userListingsContainer.scrollLeft += event.deltaY;
+});
+
+userBidsContainer.addEventListener('wheel', (event) => {
+  event.preventDefault();
+  userBidsContainer.scrollLeft += event.deltaY;
 });
